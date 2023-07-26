cd3a049edf4b5dbdecc3197787a8c2af65182e14
firefiber <service.filter@outlook.com>
2023-07-26 13:07:40 +0200

    static files set up, base css + base html set up, homepage routing setup

--- LangLearn/settings.py
+++ LangLearn/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # CUSTOM APPS
+    'main.apps.MainConfig',
     'feedback.apps.FeedbackConfig',
     'languages.apps.LanguagesConfig',
     'learning.apps.LearningConfig',
@@ -137,9 +138,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
