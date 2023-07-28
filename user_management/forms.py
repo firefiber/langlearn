a61bf99052a96948b887d5a48b51c78623ff993d
firefiber <service.filter@outlook.com>
2023-07-28 18:41:50 +0200

    user registration, account creation

--- user_management/forms.py
+++ user_management/forms.py
@@ -6,9 +6,9 @@ from django.contrib.auth.forms import UserCreationForm
 class UserForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ('username', 'password1', 'password2')
+        fields = ('username', 'email', 'password1', 'password2')
 
 class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
-        fields = ('native_language',)  # add other fields if necessary
+        fields = ('native_language', 'learning_languages')  # add other fields if necessary
--- user_management/views.py
+++ user_management/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from .forms import UserForm, UserProfileForm
 
 def register(request):
-    form = UserCreationForm()
-    return render(request, 'user_management/register.html', {'form': form})
+    if request.method == 'POST':
+        user_form = UserForm(request.POST)
+        profile_form = UserProfileForm(request.POST)
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+            profile = profile_form.save(commit=False)
+            profile.user = user
+            profile.save()
+            return redirect('login')
+    else:
+        user_form = UserForm()
+        profile_form = UserProfileForm()
+
+    return render(request, 'user_management/register.html', {'user_form': user_form, 'profile_form': profile_form})
