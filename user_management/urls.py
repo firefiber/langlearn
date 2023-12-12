94762b64429bdbd3954fd4bea9fe14c3c31eb605
firefiber <8954699+firefiber@users.noreply.github.com>
2023-12-12 01:48:00 +0100

    REST updates - Added session auth check endpoint for quick frontend auth response - Added corresponding view

--- user_management/urls.py
+++ user_management/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import CustomLoginView, CustomLogoutView, UserRegistrationView
+from .views import CustomLoginView, CustomLogoutView, UserRegistrationView, CustomSessionView
 
 urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name='register'),
     path('login/', CustomLoginView.as_view(), name='login'),
-    path('logout/', CustomLogoutView.as_view(), name='logout')
+    path('logout/', CustomLogoutView.as_view(), name='logout'),
+    path('session/', CustomSessionView.as_view(), name='session')
 ]
