ebdd3112af0dfcc8141e5965f3db6c34e237e409
firefiber <8954699+firefiber@users.noreply.github.com>
2023-12-30 15:31:37 +0100

    Settings organization - Cleaned up base settings - Reworked prod settings

--- LangLearn/settings/prod.py
+++ LangLearn/settings/prod.py
@@ -23,16 +23,20 @@ TEMPLATES = [
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 ############################################## COOKIES
 
 CORS_ALLOW_CREDENTIALS = True
+
 SESSION_COOKIE_HTTPONLY = True
 SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SAMESITE = 'None'
+
 CSRF_COOKIE_HTTPONLY = True
+CSRF_COOKIE_SECURE = True
+CSRF_COOKIE_SAMESITE = 'None'
 
 ############################################## EMAIL
 
