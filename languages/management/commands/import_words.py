e428fea3d7ba776a4a3300b59c1a136a6b511aed
firefiber <8954699+firefiber@users.noreply.github.com>
2024-05-03 16:28:13 +0200

    feat: Reset Word/Frequency id sequence. Update command to populate Word and FrequencyWordDeck tables.

--- languages/management/commands/import_words.py
+++ languages/management/commands/import_words.py
@@ -1,5 +1,6 @@
-import os
+import os, sys
 from django.core.management.base import BaseCommand
+from django.core.exceptions import ObjectDoesNotExist
 from languages.models import Word, Language
 from learning.models import FrequencyWordDeck
 import pandas as pd
@@ -8,27 +9,42 @@ import pandas as pd
 class Command(BaseCommand):
     help = 'Import words from an Excel file'
 
+    def add_arguments(self, parser):
+        parser.add_argument('language', type=str, help="Language")
+
     def handle(self, *args, **options):
-        # Get the path to the Excel file relative to the current script
-        file_path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'data', 'frequency_table.xlsx')
+        try:
+            # Get the path to the Excel file relative to the current script
+            file_path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'data', 'frequency_table.xlsx')
 
-        # Read the Excel file into a pandas DataFrame
-        df = pd.read_excel(file_path)
-        df = df[::-1]
-        # Get the language instance for the desired language (e.g., Spanish)
-        language = Language.objects.get(name='Spanish')
+            if not os.path.exists(file_path):
+                raise FileNotFoundError("File not found.")
+            # Get the language item
+            language = Language.objects.get(name=options['language'])
 
-        # Iterate over the DataFrame rows and create Word instances
-        for _, row in df.iterrows():
-            word = Word.objects.create(
-                language=language,
-                word_item=row['lemma'],
-            )
+            # Read in the file
+            df = pd.read_excel(file_path)
 
-            FrequencyWordDeck.objects.create(
-                language=language,
-                word_item=word,
-                frequency_rating=row['freq']
-            )
+            # Loop over rows in file
+            for _, row in df.iterrows():
+                # Create a Word item for each item
+                word = Word.objects.create(
+                    language=language,
+                    word_item=row['lemma']
+                )
+                frequency_word = FrequencyWordDeck.objects.create(
+                    language=word.language,
+                    word_item=word,
+                    frequency_rating=row['freq']
+                )
 
-        self.stdout.write(self.style.SUCCESS('Words imported successfully.'))
+            self.stdout.write(self.style.SUCCESS('Words and FrequencyTable populated successfully.'))
+
+        except FileNotFoundError:
+            print("File not found.")
+            sys.exit()
+
+        except ObjectDoesNotExist:
+            print("Language does not exist.")
+            sys.exit()
+
